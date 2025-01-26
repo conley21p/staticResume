6a122954fdbc5ee1fed5ec68ed22e7ce55987afa
Conley Price <conleyprice@Conleys-MacBook-Pro.local>
2025-01-26 17:11:19 -0600

    Update to Anduril resume

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using staticResume;
+using AndurilResume;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
